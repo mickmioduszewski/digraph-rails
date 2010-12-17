42f947004dc5552b0084d5083110a0ae0a8d6f4d
Mick Mioduszewski <mick@mioduszewski.net>
2010-12-18 10:43:13 +1100

    virtual attributes for edge and basic edge presentation

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,4 +1,21 @@
 class Edge < ActiveRecord::Base
   belongs_to :source, :class_name => "Node", :foreign_key => "source_id"
   belongs_to :sink, :class_name => "Node", :foreign_key => "sink_id"
+  
+  def source_name
+    source.name if source
+  end
+  
+  def source_name=(name)
+    self.source = Node.find_or_create_by_name(name) unless name.blank?
+  end
+  
+  def sink_name
+    sink.name if sink
+  end
+ 
+  def sink_name=(name)
+    self.sink = Node.find_or_create_by_name(name) unless name.blank?
+  end 
+  
 end
