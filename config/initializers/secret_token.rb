cc3ab08f3de46a5f8fa0ad7f4c1b64016071958a
Mick Mioduszewski <mick@mioduszewski.net>
2010-12-17 23:41:03 +1100

    initial empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digraph::Application.config.secret_token = '611f142feba5720652987d280d2425fe933a38ffac012ba6b9015509bfaf3c82f77a3cc85cef21bcecf59d19304379a7b037d0ce432efa4181460cf4950a9c0d'
